6470008bf17878ec1d4f29238622f9b440705533
sunil925business@gmail.com <sunil925business@gmail.com>
2025-06-04 14:52:46 +0530

    cards-moving

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2] ">
-  
-   <HeroSection/>
-   <FeaturedCourses/>
-   <WhyChooseUs/>
-   </main>
-  )
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+    </main>
+  );
 }
-
-
